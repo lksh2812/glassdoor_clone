8bcb0ce2c24ba9bcd9ac6c5198efc3932d62752d
= <=>
2019-09-27 12:42:12 +0530

    heroku deployment changes

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_moment import Moment
-from elasticsearch import Elasticsearch
+# from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,7 +15,8 @@ bootstrap = Bootstrap(app)
 loginmanager = LoginManager(app)
 loginmanager.login_view = 'login'
 moment = Moment(app)
-app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
-    if app.config['ELASTICSEARCH_URL'] else None
+# app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+#     if app.config['ELASTICSEARCH_URL'] else None
+
 
 from app import routes, models
--- config.py
+++ config.py
@@ -7,4 +7,5 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 5
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    # ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    # LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
